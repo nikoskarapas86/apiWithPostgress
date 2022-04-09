81ab88b455f6cbb83f5ad5c979e6a7c06a619d02
nikolaos karapas <nikos.karapas86@gmail.com>
2022-04-09 10:09:27 +0300

    in progress

--- src/tests/services/userService.spec.ts
+++ src/tests/services/userService.spec.ts
@@ -1,7 +1,14 @@
+import { UserInterface } from "../../interfaces/User";
 import { UserService } from "../../services/userService";
 
 const userService: UserService = new UserService();
-describe("UserService test", () => {
+describe("UserService methods in test", () => {
+  const user: UserInterface = {
+    firstName: "nik",
+    lastName: "karapas",
+    password: "fermiDirac",
+  };
+
   it(" method getUsers  should exist", () => {
     expect(userService.getUsers).toBeDefined();
   });
@@ -14,5 +21,28 @@ describe("UserService test", () => {
   });
   it("method deleteUser  should exist", () => {
     expect(userService.deleteUser).toBeDefined();
+  });
+
+  async function deleteUser(id: number) {
+    return userService.deleteUser(id);
+  }
+
+  it("create method should create a user", async () => {
+    const newUser: UserInterface = await userService.createUser(user);
+
+    if (newUser) {
+      expect(user.firstName).toBe(newUser.firstName);
+      expect(user.lastName).toBe(newUser.lastName);
+    }
+    newUser.id && (await deleteUser(newUser.id));
+  });
+  it("remove method should remove the user", async () => {
+    const newUser: UserInterface = await userService.createUser(user);
+
+    newUser.id && (await deleteUser(newUser.id));
+
+    const listOfUsers = await userService.getUsers();
+
+    expect(listOfUsers).toEqual([]);
   });
 });
